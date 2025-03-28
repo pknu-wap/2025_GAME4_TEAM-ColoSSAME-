961d01db7aa2fab3ec026224d8823ab2b275c0d4
jun3181 <rlatjdwns0109@gmail.com>
2025-03-28 15:42:44 +0900

    reset

---
+++ Main_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float attackRange = 2f;
+    public float retreatDistance = 3f;
+    public float attackDelay = 1f;
+    public LayerMask enemyLayer;
+    public LayerMask teammateLayer;
+    public Transform initialTarget;
+    public int strategyType;
+    
+
+    private Rigidbody2D rb;
+    private TargetingSystem targeting;
+    private BattleAI battleAI;
+    private MovementSystem movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targeting = GetComponent<TargetingSystem>();
+        battleAI = GetComponent<BattleAI>();
+        movement = GetComponent<MovementSystem>();
+
+        targeting.Initialize(enemyLayer);
+        movement.teammateLayer = teammateLayer;
+        battleAI.Initialize(rb, targeting, movement, attackRange, attackDelay, retreatDistance, speed);
+
+        targeting.StartTargeting();
+        battleAI.StartBattle();
+    }
+}
