fedc56ab2b26c3880b6a4420330b74e9e6503db8
BbrudnfbB <yhy34648760@gmail.com>
2025-04-03 18:56:10 +0900

    feat:훈련(버그수정)

--- Main_Project/Assets/Scripts/Data/MoneyChange.cs
+++ Main_Project/Assets/Scripts/Data/MoneyChange.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 using TMPro;
 //돈 표시
 
-public class DataChange : MonoBehaviour
+public class MoneyChange : MonoBehaviour
 {   
     //텍스트 지정
     public TextMeshProUGUI DataText;
     
     void Start()
     {
-        money = GameObject.Find("DataSaver").GetComponent<Data>().money;
+        //money = GameObject.Find("DataSaver").GetComponent<Data>().money;
     }
 
     void Update()
     {
-        DataText.text=$"돈 : {money}원";
+        //DataText.text=$"돈 : {money}원";
     }
 }
