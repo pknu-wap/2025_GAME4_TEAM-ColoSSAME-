7f6ec089029aacb83fa4314b5f8c5f69e96e4dcf
kwkim526 <polops2331@gmail.com>
2025-08-24 20:53:45 +0900

    Revert "Reapply "Fix/SPUMError""
    This reverts commit a3295b887cf38c54ed1d220b4cac4717b958f5b3.

--- Main_Project/Assets/BattleK/Scripts/Data/CharacterID.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class CharacterID : MonoBehaviour
-{
-    [Tooltip("UIDrag.characterKey와 동일해야 합니다.")]
-    public string characterKey;
-}
--- Main_Project/Assets/BattleK/Scripts/UI/Slot.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Slot : MonoBehaviour
-{
-    public bool IsOccupied = false;
-    public UIDrag Occupant;
-}
