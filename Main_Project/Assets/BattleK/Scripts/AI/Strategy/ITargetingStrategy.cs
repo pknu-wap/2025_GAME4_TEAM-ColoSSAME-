78b94abb8e07b0163df0668d0535c18e50dc9166
kwkim526 <polops2331@gmail.com>
2025-08-24 20:50:58 +0900

    Revert "Fix/SPUMError"
    This reverts commit 44a1ea821f8129778950e545e206c6fd26e64972.

--- Main_Project/Assets/BattleK/Scripts/AI/Strategy/ITargetingStrategy.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface ITargetingStrategy
-{
-    Transform FindTarget(AICore ai);
-}
--- Main_Project/Assets/BattleK/Scripts/Manager/Strategy/Runtime/PlayerStrategy/FormationAsset.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Battle/Formation/Asset", fileName = "FM_")]
-public class FormationAsset : ScriptableObject
-{
-    [Header("UI Anchored Positions (px, PreviewSlots 기준)")]
-    public Vector2[] uiAnchoredPositions;
-}
