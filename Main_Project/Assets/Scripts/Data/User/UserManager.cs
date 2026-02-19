fde168f63079f5fbcc8f44d005c946c26ca45d5a
kwkim526 <polops2331@gmail.com>
2026-02-19 23:11:50 +0900

    Fix: 가문 오류 수정
    -가문 불러오기 오류 수정 (임시 -> 기존 name 기반 형식에서 id 형식으로 수정 필요)

--- Main_Project/Assets/Scripts/Data/User/UserManager.cs
+++ Main_Project/Assets/Scripts/Data/User/UserManager.cs
@@ -192,35 +192,6 @@ public class UserManager : MonoBehaviour
             Debug.LogWarning($"❌ 제거할 아이템이 없습니다: {itemName}");
         }
     }
-    /*public void GetRandomUnit()
-    {
-        if (UnitDataManager.Instance == null)
-        {
-            Debug.LogError("❌ DataManager가 초기화되지 않았습니다.");
-            return;
-        }
-
-        // DataManager에서 로드된 모든 유닛 ID 목록을 가져온다.
-        List<string> allUnitIds = new List<string>(UnitDataManager.Instance.unitDataDict.Keys);
-        if (allUnitIds.Count == 0)
-        {
-            Debug.LogWarning("❌ 로드된 유닛 데이터가 없습니다.");
-            return;
-        }
-
-        // 목록에서 무작위로 유닛 ID를 하나 선택한다.
-        string randomUnitId = allUnitIds[Random.Range(0, allUnitIds.Count)];
-        
-        // 선택된 ID로 새로운 Unit 인스턴스를 생성한다.
-        Unit newUnit = new Unit(randomUnitId);
-
-        // 생성된 유닛을 유저의 myUnits 리스트에 추가한다.
-        AddUnit(newUnit);
-        
-        // 획득한 유닛의 이름을 DataManager를 통해 가져와 로그를 출력한다.
-        CharacterData acquiredData = UnitDataManager.Instance.GetCharacterData(randomUnitId);
-        Debug.Log($"🎉 새로운 유닛 획득! 이름: {acquiredData.Unit_Name}, 희귀도: {acquiredData.Rarity}");
-    }*/
     
     public void AddUnit(Unit newUnit)
     {
@@ -245,7 +216,6 @@ public class UserManager : MonoBehaviour
             return;
         }
 
-        // JSON 파일의 순서대로 5번부터 10번까지의 유닛을 추가한다.
         int start = 5; // 6번째 캐릭터 (인덱스 5)
         int end = start + 5; // 5개를 추가 (5, 6, 7, 8, 9, 10)
 
