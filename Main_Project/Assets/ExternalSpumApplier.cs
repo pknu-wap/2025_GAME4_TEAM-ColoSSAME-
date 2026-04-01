766104ae3760f8c82e14e61427a161d7a33d9585
pipiPAPICO <163659664+pipiPAPICO@users.noreply.github.com>
2026-04-01 19:59:02 +0900

    훈련소 기능 추가
    # 훈련소
    - 보유 캐릭터 이미지 불러오기
    - 캐릭터 선택 시 상세정보 띄우기
    - 훈련 버튼 클릭 시 레벨 별 돈 차감

--- Main_Project/Assets/ExternalSpumApplier.cs
+++ Main_Project/Assets/ExternalSpumApplier.cs
@@ -2,7 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class ExternalSpumApplier : MonoBehaviour
+public class 
+    ExternalSpumApplier : MonoBehaviour
 {
     public GameObject spumCharacterPrefab;
     public List<PreviewMatchingElement> partsToApply;
