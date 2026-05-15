48fdb6d057b884e76799eb76d1a323023a071f3b
wonjiee <wonjiee01@gmail.com>
2026-05-15 14:13:15 +0900

    임시

---
+++ Main_Project/Assets/Scripts/Data/Enemy/EnemyTeam.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTeam
+{
+    public string teamId;
+    public string teamName
+
+    public int familyId;
+    public string familyName;
+    
+    public int money;
+    
+    public List<Unit> units = new List<Unit>();
+}
