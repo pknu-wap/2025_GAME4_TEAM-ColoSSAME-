3984b47b0531115319ed4636461d9c0dd1effd1c
wonjiee <wonjiee01@gmail.com>
2026-05-17 19:45:43 +0900

    리그 상대팀 세이브 구조 추가 및 현재 경기 정보 저장 설계

--- Main_Project/Assets/Scripts/Data/Enemy/EnemyTeam.cs
+++ Main_Project/Assets/Scripts/Data/Enemy/EnemyTeam.cs
@@ -2,15 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class EnemyTeam
 {
-    public string teamId;
-    public string teamName
-
-    public int familyId;
-    public string familyName;
+    public int id;
+    public string fid;
+    public string name;
     
     public int money;
+    public int growthStage;
     
     public List<Unit> units = new List<Unit>();
+    
+    public EnemyTeam()
+
+    {
+
+    }
+
+    public EnemyTeam(int id, string fid, string name)
+
+    {
+
+        this.id = id;
+
+        this.fid = fid;
+
+        this.name = name;
+
+        this.money = 0;
+
+        this.growthStage = 0;
+
+        this.units = new List<Unit>();
+
+    }
 }
