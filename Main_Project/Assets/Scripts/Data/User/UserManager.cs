0453151bd606a136e0bf0d88a9c8a0d28cee0afc
BbrudnfbB <yhy34648760@gmail.com>
2026-09-09 16:00:19 +0900

    주석 제거

--- Main_Project/Assets/Scripts/Data/User/UserManager.cs
+++ Main_Project/Assets/Scripts/Data/User/UserManager.cs
@@ -5,13 +5,10 @@ using Newtonsoft.Json;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
-//수정
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using System.Collections;
 using BattleK.Scripts.Data.Stat;
-
-//
 
 public class UserManager : MonoBehaviour
 {
@@ -43,7 +40,7 @@ public class UserManager : MonoBehaviour
     // 현재 UI에서 선택된 유닛 ID (fighter 클릭 시 저장)
     public string selectedUnitId { get; private set; }
 
-// 선택된 유닛을 저장하는 함수
+    // 선택된 유닛을 저장하는 함수
     public void SetSelectedUnit(string unitId)
     {
         selectedUnitId = unitId;
@@ -104,8 +101,6 @@ public class UserManager : MonoBehaviour
 
         if (unit.Tier > 5)
             unit.Tier = 5;
-
-        //SaveUser();
 
         return true;
     }
